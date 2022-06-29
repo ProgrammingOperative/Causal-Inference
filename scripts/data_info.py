df123a02c4a964adb7d074d98c73a3f9ab889d7b
waCira <wachura11t@gmail.com>
2022-06-29 09:33:17 +0300

    Create the data_info class

---
+++ scripts/data_info.py
@@ -0,0 +1,18 @@
+from pyteal import Int
+
+
+class DataInfor:
+    def __init__(self, df) -> Int:
+        self.df = df
+
+    def null_percentage(self):
+        '''
+        Display Total Null percentage of the Data Frame
+        '''
+
+        number_of_rows, number_of_columns = self.df.shape
+        df_size = number_of_rows * number_of_columns
+        
+        null_size = (self.df.isnull().sum()).sum()
+        percentage = round((null_size / df_size) * 100, 2)
+        return f"Data Frame contain null values of { percentage }"
