131f89370f1403d8aff9ea359f8f10059b37aa2e
waCira <wachura11t@gmail.com>
2022-06-29 15:40:39 +0300

    Fixed outliers

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,29 +1,16 @@
+from collections import Counter
+import numpy as np
+
 class DataClean:
     def __init__(self, df) -> None:
         self.df = df
     
-    def fix_outliers(self, df, features):
-        outlier_indices = []
-        
-        for c in features:
-            # 1st quartile
-            Q1 = np.percentile(df[c],25)
-            # 3rd quartile
-            Q3 = np.percentile(df[c],75)
-            # IQR
-            IQR = Q3 - Q1
-            # Outlier step
-            outlier_step = IQR * 1.5
-            # detect outlier and their indeces
-            outlier_list_col = df[(df[c] < Q1 - outlier_step) | (df[c] > Q3 + outlier_step)].index
-            # store indeces
-            outlier_indices.extend(outlier_list_col)
+    def fix_outliers(self):
+        column_name=list(self.df.columns[2:])
+        for col in column_name:
+                lower_quartile, upper_quartile = self.df[col].quantile(0.25), self.df[col].quantile(0.75)
+                self.df[col]=np.where(self.df[col]>upper_quartile,self.df[col].median(),np.where(self.df[col]<lower_quartile,self.df[col].median(),self.df[col]))
+        return self.df
 
-        outlier_indices = Counter(outlier_indices)
+        
     
-        multiple_outliers = []
-        for v in outlier_indices.items(): 
-            if v[-1]>2:
-                multiple_outliers.append(v[0])
-            
-        return multiple_outliers
